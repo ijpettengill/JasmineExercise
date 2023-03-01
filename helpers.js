2409f730df719d7dc364046472e6e18ab612f367
Issaac Pettengill <issaac@Issaacs-iMac.lan>
2023-02-28 19:03:31 -0500

    make helpers test and fixed helpers

--- helpers.js
+++ helpers.js
@@ -1,5 +1,3 @@
-
-// accepts 'tipAmt', 'billAmt', 'tipPercent' and sums total from allPayments objects
 function sumPaymentTotal(type) {
   let total = 0;
 
@@ -12,15 +10,32 @@ function sumPaymentTotal(type) {
   return total;
 }
 
-// converts the bill and tip amount into a tip percent
 function calculateTipPercent(billAmt, tipAmt) {
   return Math.round(100 / (billAmt / tipAmt));
 }
 
-// expects a table row element, appends a newly created td element from the value
 function appendTd(tr, value) {
   let newTd = document.createElement('td');
   newTd.innerText = value;
 
   tr.append(newTd);
+}
+
+function appendDeleteBtn(tr, type) {
+  let newTd = document.createElement('td');
+  newTd.className = 'deleteBtn';
+  newTd.innerText = 'X';
+
+  newTd.addEventListener('click', removeEle);
+
+  tr.append(newTd);
+}
+
+function removeEle(evt) {
+  let ele = evt.target.closest('tr');
+
+  delete allServers[ele.id];
+
+  ele.parentNode.removeChild(ele);
+  updateServerTable();
 }
